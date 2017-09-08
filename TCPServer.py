ecc2ab98c203a0fce5349a803a2cb37039a0e7e5
Andrew <neumann6994@gmail.com>
2017-09-08 15:26:38 -0500

    added tcpserver and client

---
+++ TCPServer.py
@@ -0,0 +1,34 @@
+import socket
+import threading 
+
+
+bind_ip = "127.0.0.1"
+bind_port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip,bind_port))
+
+server.listen(5)
+
+print "[*] Listening on %s:%d" % (bind_ip,bind_port)
+def handle_client(client_socket):
+
+    #reads a 1024 bit packet
+    request = client_socket.recv(1024)
+
+    print "Action report: %s" % request
+
+    #send back a packet
+    client_socket.send("ROG!")
+
+    client_socket.close()
+
+while True:
+
+    client,addr = server.accept()   
+
+    print "[!] Message from the front %s:%d" % (addr[0],addr[1])
+
+    client_handler = threading.Thread(target=handle_client,args=(client,))
+    client_handler.start()
